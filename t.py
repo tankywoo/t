2163bbbbe1b9e118e2111c265236cffdf07d3f09
TankyWoo <wtq1990@gmail.com>
2013-05-11 17:04:55 +0800

    add arg parser

--- t.py
+++ t.py
@@ -30,11 +30,32 @@ except ImportError, e:
     print "argparse module missing: Please run 'sudo easy_install argparse'"
     sys.exit(1)
     # Python Version >= 2.7
-    # import optparse as argparse
+    # TODO:import optparse as argparse
+
+
+class TaskT():
+    pass
 
 
 def get_args():
-    pass
+    usage = '%(prog)s [options] [TEXT]'
+    #parser = argparse.ArgumentParser(description='Command-line todo - t Help')
+    parser = argparse.ArgumentParser(description='Command-line todo - t Help',
+            usage = usage)
+
+    action_group = parser.add_argument_group('Action')
+    action_group.add_argument('add', default=[], nargs='*', help='Add a task')
+    action_group.add_argument('-e', '--edit', dest='edit', default='', help='Edit a task')
+    action_group.add_argument('-f', '--finish', dest='finish', help='Finish a task')
+    action_group.add_argument('-r', '--remove', dest='remove', help='Remove a task')
+
+    output_group = parser.add_argument_group('Output')
+    output_group.add_argument('-g', '--grep', help='Grep the keyword in task box')
+    output_group.add_argument('--done', help='List all the done tasks')
+
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == '__main__':
-    pass
+    args = get_args()
